28da4704c2dfcc2eb692d25d2b90453932b3e5f4
Pham Quoc An <an.pham.quoc@preciofishbone.se>
2023-02-22 17:08:22 +0700

    Add redirect url for signout button

--- src/scripts/pages/home-page.ts
+++ src/scripts/pages/home-page.ts
@@ -1,7 +1,6 @@
-import { FileGenerated } from '../../models/_file';
-import { FolderGenerated } from '../../models/_folder';
 import fileService from '../../services/file';
 import folderService from '../../services/folder';
+import userService from '../../services/user';
 import renderGrid from '../components/_grid';
 import modalHelper from '../components/_modal';
 import tableHelper from '../components/_table';
@@ -9,35 +8,27 @@ import ready, { convertToJSONString } from '../utilities/_helper';
 
 declare var $: any;
 
-const indexLinkFolder: number[] = JSON.parse(
-  localStorage.getItem('indexLinkFolder') || '[1]',
+const indexLinkFolder: string[] = JSON.parse(
+  localStorage.getItem('indexLinkFolder') ||
+    '["D0D0AADD-B674-4434-09F0-08DB12FD2CFE"]',
 );
 
-const indexFolder = indexLinkFolder[indexLinkFolder.length - 1] || 1;
+const indexFolder =
+  indexLinkFolder[indexLinkFolder.length - 1] ||
+  'D0D0AADD-B674-4434-09F0-08DB12FD2CFE';
 
 ready(() => {
   renderGrid();
 });
 
-$(document).ready(() => {
-  // Waiting loading content
-  $('.table-container').html('Loading...');
-  $('#container-header-title').html('Loading...');
-
-  // hide icon back when there is a root folder
-  if (indexLinkFolder.length === 1) {
-    $('#backIcon').hide();
-  }
-
+const renderTable = async () => {
   folderService.getById(indexFolder).then(folder => {
-    $('.table-container').html(
-      folder !== undefined ? '' : 'Folder not found!',
-    );
+    $('.table-container').html(folder ? '' : 'Folder not found!');
     $('#container-header-title').html(
-      folder !== undefined ? '' : 'Folder not found!',
+      folder ? '' : 'Folder not found!',
     );
 
-    if (folder === undefined) return;
+    if (!folder) return;
 
     // in case user clicks to folder
     $('#backIcon').on('click', () => {
@@ -65,133 +56,180 @@ $(document).ready(() => {
       $('.table-container').append($FILE_ITEM);
     });
   });
+};
 
-  // When user click to new item button
-  $('#btnNewItem').on('click', () => {
-    $('#btnSubmit').data('type', 'add');
+$(document).ready(() => {
+  userService.getUserInfo().then(user => {
+    $('#username').text(user.name);
+    // Waiting loading content
+    $('.table-container').html('Loading...');
+    $('#container-header-title').html('Loading...');
 
-    modalHelper.setValueForCommonModal('Add', 'folder', '', '', 0);
-  });
+    // handle file
+    $('#file-input').change((e: any) => {
+      const file = e.target.files[0];
+      if (!file) return;
 
-  // when modal is hidden
-  $('#commonModal').on('hidden.bs.modal', () => {
-    modalHelper.resetCommonModal();
-  });
-
-  // add new item
-  $('#btnSubmit').on('click', () => {
-    const type = $('#type').val();
-    const name = $('#name').val();
-    const extension = $('#extension').val();
-    const id = parseInt($('#id').val(), 10);
-    const btnType = $('#btnSubmit').data('type');
-
-    // handle edit and add
-    switch (btnType) {
-      case 'add': {
-        if (type === 'file') {
-          const newFile = FileGenerated({
-            name,
-            extension,
-          });
-          modalHelper.setLoadingForSubmitButton(true);
-
-          folderService.addFile(indexFolder, newFile).then(res => {
-            if (!res) return;
-
-            modalHelper.setLoadingForSubmitButton(false);
-            $('.table-container').append(
-              tableHelper.renderTableItem(newFile),
-            );
-
-            $('#commonModal').modal('hide');
-          });
-        } else {
-          const newFolder = FolderGenerated({
-            name,
-          });
-
-          modalHelper.setLoadingForSubmitButton(true);
-
-          folderService.create(indexFolder, newFolder).then(res => {
-            modalHelper.setLoadingForSubmitButton(false);
-            if (!res) return;
-
-            const $FOLDER_ITEM = tableHelper.renderTableItem(
-              newFolder,
-            );
-            $('.table-container').prepend($FOLDER_ITEM);
-
-            $('#commonModal').modal('hide');
-          });
-        }
-        break;
+      // maximum file size is 10MB
+      if (file.size > 10 * 1024 * 1024) {
+        $('#toast-message').text('File size is too large!');
+        $('#notifyToast').toast('show');
+        return;
       }
-      case 'edit': {
-        // Edit file
-        if (type === 'file') {
-          modalHelper.setLoadingForSubmitButton(true);
+      const name = file.name.split('.')[0];
+      const extension = file.name.split('.')[1];
+      folderService
+        .addFile(indexFolder, {
+          name,
+          extension,
+          modifiedBy: 'admin',
+        })
+        .then(res => {
+          if (!res) return;
 
-          fileService
-            .update(indexFolder, {
-              modifiedAt: new Date(),
-              name,
-              extension,
-              id,
-            })
-            .then(res => {
-              modalHelper.setLoadingForSubmitButton(false);
-              if (!res) return;
-              const $FILE_ITEM = tableHelper.renderTableItem(res);
+          $('.table-container').append(
+            tableHelper.renderTableItem(res),
+          );
+        });
+    });
 
-              $(`.table-item[data-id="${id}"]`).replaceWith(
-                $FILE_ITEM,
-              );
-
-              $('#commonModal').modal('hide');
-            });
-        }
-        // Edit folder
-        else {
-          modalHelper.setLoadingForSubmitButton(true);
-          folderService
-            .update(
-              id,
-              {
-                modifiedAt: new Date(),
-                name,
-              },
-              indexFolder,
-            )
-            .then(res => {
-              if (!res) return;
-
-              modalHelper.setLoadingForSubmitButton(false);
-              const $FOLDER_ITEM = tableHelper.renderTableItem(res);
-
-              $(`.table-item[data-id="${id}"]`).replaceWith(
-                $FOLDER_ITEM,
-              );
-
-              $('#commonModal').modal('hide');
-            });
-        }
-        break;
-      }
-      default: {
-        // statements;
-        break;
-      }
+    // hide icon back when there is a root folder
+    if (indexLinkFolder.length === 1) {
+      $('#backIcon').hide();
     }
-  });
 
-  // When click to change type of item
-  $('#type').on('change', () => {
-    const type = $('#type').val();
+    renderTable();
 
-    $('#extension').prop('disabled', type === 'folder');
-    $('#commonModal')
-      .find('#commonModalLabel')
-      .text(`Add ${type}`);
+    // set function for sync button
+    $('#syncBtn').click(() => {
+      renderTable();
+    });
+
+    // When user click to new item button
+    $('#btnNewItem').on('click', () => {
+      $('#btnSubmit').data('type', 'add');
+
+      modalHelper.setValueForCommonModal('Add', 'folder', '', '', '');
+    });
+
+    $('#logoutBtn').click(() => {
+      window.location.href =
+        'https://localhost:44307/api/Auth/signout';
+    });
+
+    // add new item
+    $('#btnSubmit').on('click', () => {
+      const type = $('#type').val();
+      const name = $('#name').val();
+      const extension = $('#extension').val();
+      const id = $('#id').val();
+      const btnType = $('#btnSubmit').data('type');
+
+      // handle edit and add
+      switch (btnType) {
+        case 'add': {
+          if (type === 'file') {
+            modalHelper.setLoadingForSubmitButton(true);
+
+            folderService
+              .addFile(indexFolder, {
+                name,
+                extension,
+                modifiedBy: 'admin',
+              })
+              .then(res => {
+                if (!res) return;
+
+                modalHelper.setLoadingForSubmitButton(false);
+                $('.table-container').append(
+                  tableHelper.renderTableItem(res),
+                );
+
+                $('#commonModal').modal('hide');
+              });
+          } else {
+            modalHelper.setLoadingForSubmitButton(true);
+
+            folderService
+              .create(indexFolder, {
+                name,
+                modifiedBy: 'admin',
+              })
+              .then(res => {
+                modalHelper.setLoadingForSubmitButton(false);
+                if (!res) return;
+
+                const $FOLDER_ITEM = tableHelper.renderTableItem(res);
+                $('.table-container').prepend($FOLDER_ITEM);
+
+                $('#commonModal').modal('hide');
+              });
+          }
+          break;
+        }
+        case 'edit': {
+          // Edit file
+          if (type === 'file') {
+            modalHelper.setLoadingForSubmitButton(true);
+
+            fileService
+              .update({
+                modifiedAt: new Date(),
+                modifiedBy: 'admin',
+                name,
+                extension,
+                id,
+              })
+              .then(res => {
+                modalHelper.setLoadingForSubmitButton(false);
+                if (!res) return;
+                const $FILE_ITEM = tableHelper.renderTableItem(res);
+
+                $(`.table-item[data-id="${id}"]`).replaceWith(
+                  $FILE_ITEM,
+                );
+
+                $('#commonModal').modal('hide');
+              });
+          }
+          // Edit folder
+          else {
+            modalHelper.setLoadingForSubmitButton(true);
+            folderService
+              .update(id, {
+                modifiedAt: new Date(),
+                modifiedBy: 'admin',
+                name,
+              })
+              .then(res => {
+                if (!res) return;
+
+                modalHelper.setLoadingForSubmitButton(false);
+                const $FOLDER_ITEM = tableHelper.renderTableItem(res);
+
+                $(`.table-item[data-id="${id}"]`).replaceWith(
+                  $FOLDER_ITEM,
+                );
+
+                $('#commonModal').modal('hide');
+              });
+          }
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+
+    // When click to change type of item
+    $('#type').on('change', () => {
+      const type = $('#type').val();
+
+      $('#extension').prop('disabled', type === 'folder');
+      $('#commonModal')
+        .find('#commonModalLabel')
+        .text(`Add ${type}`);
+    });
   });
 });
